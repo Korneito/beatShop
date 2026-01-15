5eb8ff47cad7e9f99b40e99b4bed7d44f30f8794
Korneito <vladik.kor444@gmail.com>
2026-01-15 21:09:06 +0100

    Add Contact page

--- src/components/BeatCard.vue
+++ src/components/BeatCard.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="w-100 h-25 border-3 border-red-600  bg-zinc-800 flex justify-center items-center rounded-2xl  hover:bg-white transition hover:text-zinc-800 " >
-
-        <div class="flex items-center justify-center w-full p-10">
-            {{ beat.title }}
+        
+        <div class="flex items-center justify-between w-full p-10">
+          <img src="/beat.png" alt="" class="w-20 h-25 object-contain">
+            <p class="pl-5">{{ beat.title }}</p>
         </div>
         <div class="flex items-center justify-center w-full p-10">
             {{ beat.price }}
